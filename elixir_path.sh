e9f5fa46993c85cd69f8d0c1b2a6a909cc38c91f
Low Kian Seong <kianseong@gmail.com>
2015-03-17 17:47:13 +0800

    Delete elixir_path.sh

--- elixir_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-sudo sh -c "cat > /etc/sudoers.d/secure_path" <<'EOF'
-Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/vagrant/vendor/elixir/bin/"
-EOF
